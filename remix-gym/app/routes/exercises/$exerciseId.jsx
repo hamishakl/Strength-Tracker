4f0c6a21a80cde3a89b733a77bc9bfa967d839af
Hamish Henare <hamishjustin7@gmail.com>
2022-01-28 17:08:39 +1300

    can delete exercises from db now

--- remix-gym/app/routes/exercises/$exerciseId.jsx
+++ remix-gym/app/routes/exercises/$exerciseId.jsx
@@ -1,14 +1,32 @@
-import { useLoaderData } from "remix";
+import { redirect, useLoaderData, Link } from "remix";
 import { db } from "~/utils/db.server";
-import {Link} from 'remix'
 
 export const loader = async ({ params }) => {
   const exercise = await db.exercise.findUnique({
     where: { id: params.exerciseId },
   });
 
+  if (!exercise) {
+    throw new Error("Exercise not found");
+  }
+
   const data = { exercise };
   return data;
+};
+
+export const action = async ({ request, params }) => {
+  const form = await request.formData();
+  if (form.get("_method") === "delete") {
+    const exercise = await db.exercise.findUnique({
+      where: { id: params.exerciseId },
+    });
+
+    if (!exercise) {
+      throw new Error("Exercise not found");
+    }
+    await db.exercise.delete({ where: { id: params.exerciseId } });
+    return redirect("/exercises");
+  }
 };
 
 function Exercise() {
@@ -20,6 +38,12 @@ function Exercise() {
         <Link to="/exercises" className="btn btn-reverse">
           Back
         </Link>
+      </div>
+      <div className="page-footer">
+        <form method="POST">
+          <input type="hidden" name="_method" value="delete" />
+          <button className="btn btn-delete">Delete</button>
+        </form>
       </div>
     </div>
   );

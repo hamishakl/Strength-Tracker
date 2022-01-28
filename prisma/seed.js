2ae56356872507b3954e187d2beba2fd936541e0
Hamish Henare <hamishjustin7@gmail.com>
2022-01-28 18:58:33 +1300

    PR db field added

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,6 +5,9 @@ async function seed() {
   await Promise.all(
     getExercises().map((exercise) => {
       return db.exercise.create({ data: exercise });
+    }),
+    getPrs().map((pr) => {
+      return db.pr.create({ data: pr });
     })
   );
 }
@@ -14,6 +17,12 @@ function getExercises() {
     { title: "Squat", body: "Barbell squat" },
     { title: "Bench", body: "Barbell flat bench press" },
     { title: "Deadlift", body: "Barbell conventional deadlift" },
+  ];
+}
+
+function getPrs() {
+  return [
+    { exercise: "Squat", weight: 50, reps: 5, sets: 3  }
   ];
 }
 

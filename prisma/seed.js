9127fe57789e5c422286df5bec85df7e06ba619b
Hamish Henare <hamishjustin7@gmail.com>
2022-01-28 15:50:49 +1300

    prisma sql database loaded with seed file

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,8 +3,20 @@ const {
 } = require('@prisma/client')
 const db = new PrismaClient()
 
-function getPosts() {
+async function seed() {
+    await Promise.all(
+        getExercises().map(exercise => {
+            return db.post.create({ data: exercise})
+        })
+    )
+}
+
+function getExercises() {
     return [
-        
+        { title: "Squat", body: "Barbell squat" },
+      { title: "Bench", body: "Barbell flat bench press" },
+      { title: "Deadlift", body: "Barbell conventional deadlift" },
     ]
 }
+
+seed()

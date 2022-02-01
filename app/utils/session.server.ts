5f4bb36159151ba0df1c10d9d785888837004508
Hamish Henare <hamishjustin7@gmail.com>
2022-02-02 08:17:34 +1300

    added login cookie

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -20,3 +20,35 @@ export async function login({username, password}) {
 
     return user
 }
+
+const sessionSecret = process.env.SESSION_SECRET 
+if(!sessionSecret) {
+    throw new Error('No session secret')
+}
+
+const storage = createCookieSessionStorage({
+    cookie: {
+        name: 'remixblog_session', 
+        secure: process.env.NODE_ENV === 'production',
+        secrets: [sessionSecret],
+        sameSite: 'lax',
+        path: '/',
+        maxAge: 60 * 60 * 24 * 60,
+        httpOnly: true
+    }
+})
+
+export async function createUserSession(userId:string, redirectTo: string) {
+    const session = await storage.getSession()
+    session.set(
+        'userId', userId
+    )
+    return redirect(
+        redirectTo, {
+            headers: {
+                'Set-Cookie': await storage.commitSession(session)
+            }
+        }
+    )
+}
+

f879767f60ed3fd5d155055131ac1f7dcb477292
Hamish Henare <hamishjustin7@gmail.com>
2022-02-04 12:46:30 +1300

    fixed up db for user, pr and exercise

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,9 +2,9 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 async function seed() {
-  const hamishs = await prisma.user.create({
+  const adminz = await prisma.user.create({
     data: {
-      username: 'hamishs',
+      username: 'adminz',
       passwordHash:
         '$2b$10$K7L1OJ45/4Y2nIvhRVpCe.FSmhDdWoXehVzJptJ/op0lSsvqNu/1u',
     },
@@ -13,7 +13,7 @@ async function seed() {
 
   await Promise.all(
     getExercises().map((exercise) => {
-      const data = { userId: hamishs.id, ...exercise }
+      const data = { userId: adminz.id, ...exercise }
       return prisma.exercise.create({ data })
     })
   )
@@ -28,10 +28,3 @@ function getExercises() {
     { title: "Deadlift", body: "Barbell conventional deadlift" },
   ];
 }
-
-// function getPrs() {
-//   return [
-//     { exercise: "Squat", weight: 50, reps: 5, sets: 3  }
-//   ];
-// }
-

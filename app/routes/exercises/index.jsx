d2e814b5bf8403ca77e0f2c130e025198506fc53
Hamish Henare <hamishjustin7@gmail.com>
2022-02-06 13:50:57 +1300

    linked up pr db to render

--- app/routes/exercises/index.jsx
+++ app/routes/exercises/index.jsx
@@ -1,40 +1,39 @@
-import { Link, useLoaderData } from 'remix'
-import { db } from '~/utils/db.server'
+import { Link, useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
 
 import { getUser } from "~/utils/session.server";
 
-
 export const loader = async ({ request }) => {
-
   const user = await getUser(request);
-
 
   const data = {
     exercises: await db.exercise.findMany({
-      where: { userId: {
-        equals: `${user.id}`
-      } },
+      where: {
+        userId: {
+          equals: `${user.id}`,
+        },
+      },
       take: 20,
       select: { id: true, title: true, createdAt: true },
-      orderBy: { createdAt: 'desc' },
+      orderBy: { createdAt: "desc" },
     }),
-  }
+  };
 
-  return data
-}
+  return data;
+};
 
 function ExerciseItems() {
-  const { exercises } = useLoaderData()
+  const { exercises } = useLoaderData();
 
   return (
     <>
-      <div className='page-header'>
+      <div className="page-header">
         <h1>exercises</h1>
-        <Link to='/exercises/new' className='btn'>
+        <Link to="/exercises/new" className="btn">
           New exercise
         </Link>
       </div>
-      <ul className='exercises-list'>
+      <ul className="exercises-list">
         {exercises.map((exercise) => (
           <li key={exercise.id}>
             <Link to={exercise.id}>
@@ -45,7 +44,7 @@ function ExerciseItems() {
         ))}
       </ul>
     </>
-  )
+  );
 }
 
-export default ExerciseItems
+export default ExerciseItems;

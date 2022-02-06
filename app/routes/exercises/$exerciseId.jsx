f140093f7953fc3270a05e99309fd5ae1194dc65
Hamish Henare <hamishjustin7@gmail.com>
2022-02-06 18:42:12 +1300

    prs now render according to exercise

--- app/routes/exercises/$exerciseId.jsx
+++ app/routes/exercises/$exerciseId.jsx
@@ -1,72 +1,87 @@
-import { Link, redirect, useLoaderData } from 'remix'
-import { db } from '~/utils/db.server'
-import { getUser } from '~/utils/session.server'
+import { Link, redirect, useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+import { getUser } from "~/utils/session.server";
+import PrItems from "./pr";
 
 export const loader = async ({ request, params }) => {
-  const user = await getUser(request)
-  console.log(params.exerciseId)
+  const user = await getUser(request);
+  console.log(params.exerciseId);
   const exercise = await db.exercise.findUnique({
     where: { id: params.exerciseId },
-  })
+  });
+
+  if (!exercise) throw new Error("exercise not found");
 
   const pr = await db.pr.findMany({
-    where: { exerciseId:{
-      equals: params.exerciseId
-    }}
-  })
+    where: {
+      userId: {
+        equals: `${user.id}`,
+      },
+      exerciseId: {
+        equals: `${exercise.id}`,
+      },
+    },
+  });
 
-  if (!exercise) throw new Error('exercise not found')
-
-  const data = { exercise, user, pr }
-  return data
-}
+  const data = { exercise, user, pr };
+  return data;
+};
 
 export const action = async ({ request, params }) => {
-  const form = await request.formData()
-  if (form.get('_method') === 'delete') {
-    const user = await getUser(request)
+  const form = await request.formData();
+  if (form.get("_method") === "delete") {
+    const user = await getUser(request);
 
     const exercise = await db.exercise.findUnique({
       where: { id: params.exerciseId },
-    })
+    });
 
-    if (!exercise) throw new Error('exercise not found')
+    if (!exercise) throw new Error("exercise not found");
 
     if (user && exercise.userId === user.id) {
-      await db.exercise.delete({ where: { id: params.exerciseId } })
+      await db.exercise.delete({ where: { id: params.exerciseId } });
     }
 
-    return redirect('/exercises')
+    return redirect("/exercises");
   }
-}
+};
 
 function exercise() {
-  const { exercise, user, pr } = useLoaderData()
-  console.log(pr)
-  return (
+  const { exercise, user, pr } = useLoaderData();
 
+  return (
     <div>
-      <div className='page-header'>
+      <div className="page-header">
         <h1>{exercise.title}</h1>
-        <Link to='/exercises' className='btn btn-reverse'>
+        <Link to="/exercises" className="btn btn-reverse">
           Back
         </Link>
-      </div> 
-
-      <div className='page-content'>
-      <h1>{pr[0].weight}</h1>
-        {exercise.body}
       </div>
-      <div className='page-footer'>
+
+      <ul className="pr-list">
+        {pr.map((pr) => (
+          <li key={pr.id}>
+            <Link to={pr.id}>
+              <h3>{pr.weight}</h3>
+              <h3>{pr.reps}</h3>
+              <h3>{pr.sets}</h3>
+              {new Date(pr.createdAt).toLocaleString()}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <div className="page-content">{exercise.body}</div>
+      <div className="page-footer">
         {user.id === exercise.userId && (
-          <form method='POST'>
-            <input type='hidden' name='_method' value='delete' />
-            <button className='btn btn-delete'>Delete</button>
+          <form method="POST">
+            <input type="hidden" name="_method" value="delete" />
+            <button className="btn btn-delete">Delete</button>
           </form>
         )}
       </div>
     </div>
-  )
+  );
 }
 
-export default exercise
+export default exercise;

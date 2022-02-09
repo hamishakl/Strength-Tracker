697bddcf9ca0440a9cb8b0f95a3b76eea6e46921
Hamish Henare <hamishjustin7@gmail.com>
2022-02-10 10:19:39 +1300

    can view idnividual prs now

---
+++ app/routes/exercises/$exerciseId.$pr.jsx
@@ -0,0 +1,52 @@
+import { Link, redirect, useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+import { getUser } from "~/utils/session.server";
+import { OneRmEstimate } from './$exerciseId'
+
+export const loader = async ({ request, params }) => {
+  // console.log(params);
+  const user = await getUser(request);
+  const exercise = await db.exercise.findUnique({
+    where: { id: params.exerciseId },
+  });
+
+  if (!exercise) throw new Error("exercise not found");
+
+  const pr = await db.pr.findUnique({
+    where: {
+      id: params.pr,
+    },
+  });
+
+  const data = { exercise, user, pr };
+  return data;
+};
+
+function prPage() {
+  const { exercise, user, pr } = useLoaderData();
+  console.log(pr);
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>Weight</td>
+          <td>{pr.weight}</td>
+        </tr>
+        <tr>
+          <td>Reps</td>
+          <td>{pr.reps}</td>
+        </tr>
+        <tr>
+          <td>Projected 1rm</td>
+          <td>{OneRmEstimate(pr.weight, pr.reps)}</td>
+        </tr>
+        <tr>
+          <td>Date</td>
+          <td>{new Date(pr.createdAt).toLocaleString()}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default prPage;

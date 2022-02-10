9d7e4be7a1378ec1a55b5ab6f9a3a5dbe4b44ef5
Hamish Henare <hamishjustin7@gmail.com>
2022-02-10 19:24:06 +1300

    adjusted some styling for navbar

--- app/root.jsx
+++ app/root.jsx
@@ -1,16 +1,25 @@
 import { Outlet, LiveReload, Link, Links, useLoaderData } from "remix";
-import globalStylesUrl from '~/styles/global.css'
-import { getUser } from './utils/session.server'
+import globalStylesUrl from "~/styles/global.css";
+import { getUser } from "./utils/session.server";
 
-export const links = () => [{ rel: 'stylesheet', href: globalStylesUrl}]
+export const links = () => [
+  {
+    rel: "stylesheet",
+    href: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+    rel: "stylesheet",
+    integrity:
+      "sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3",
+    crossorigin: "anonymous",
+  },
+];
 
 export const loader = async ({ request }) => {
-  const user = await getUser(request)
+  const user = await getUser(request);
   const data = {
-    user
-  }
-  return data
-}
+    user,
+  };
+  return data;
+};
 
 export default function App(params) {
   return (
@@ -23,7 +32,6 @@ export default function App(params) {
 }
 
 function Document({ children, title }) {
-
   return (
     <html lang="en">
       <head>
@@ -31,7 +39,7 @@ function Document({ children, title }) {
         <title>Gym App</title>
       </head>
       <body>
-      {children}
+        {children}
         {process.env.NODE_ENV === "development" ? <LiveReload /> : null}
       </body>
     </html>
@@ -39,13 +47,11 @@ function Document({ children, title }) {
 }
 
 function Layout({ children }) {
-
-  const {user} = useLoaderData()
-
+  const { user } = useLoaderData();
 
   return (
     <>
-      <nav className="navbar">
+      {/* <nav className="navbar">
         <Link to="/" className="logo">
           Gym App
         </Link>
@@ -55,18 +61,55 @@ function Layout({ children }) {
           </li>
           {user ? (
             <li>
-              <form action='/auth/logout' method='POST'>
+              <form action="/auth/logout" method="POST">
                 <button className="btn" type="submit">
                   Logout {user.username}
                 </button>
               </form>
             </li>
           ) : (
-          <li>
-            <Link to="/auth/login">Login</Link>
-          </li>
+            <li>
+              <Link to="/auth/login">Login</Link>
+            </li>
           )}
         </ul>
+      </nav> */}
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <Link class="navbar-brand" to="/">
+            Strength Tracker
+          </Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <Link class="nav-item nav-link" to="/exercises">
+                New PR
+              </Link>
+              <Link class="nav-item nav-link" to="/exercises">
+                My Exercises
+              </Link>
+            </ul>
+          </div>
+        </div>
+        {user ? (
+              <form action="/auth/logout" method="POST">
+                <button class="btn btn-primary" type="submit">
+                  Logout {user.username}
+                </button>
+              </form>
+          ) : (
+              <Link to="/auth/login">Login</Link>
+          )}
       </nav>
       <div className="container">{children}</div>
     </>

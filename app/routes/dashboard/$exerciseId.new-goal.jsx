97b0dafb9b3298ee9e5a42a4aa0e5708f73eb695
Hamish Henare <hamishjustin7@gmail.com>
2022-02-13 22:22:18 +1300

    can add a goal now for individual exercises

--- app/routes/dashboard/$exerciseId.new-goal.jsx
+++ app/routes/dashboard/$exerciseId.new-goal.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import { db } from "~/utils/db.server";
+import { Link, redirect, useActionData, json, useLoaderData, Form } from "remix";
+
+export const loader = async ({ params }) => {
+  console.log(params);
+
+  const exercise = await db.exercise.findUnique({
+    where: {
+      id: params.exerciseId,
+    },
+  });
+
+  console.log(exercise);
+  return exercise;
+};
+
+export const action = async ({ request, params }) => {
+    const form = await request.formData();
+    const weightStr = form.get("weight");
+    const goal = parseInt(weightStr);
+    
+
+  
+    const updateUser = await db.exercise.update({
+        where: {
+          id: params.exerciseId,
+        },
+        data: {
+          goal: goal,
+        },
+      })
+  
+    return redirect(`/dashboard/${params.exerciseId}`);
+  };
+
 
 export default function NewGoal() {
-  return (
-    <div>NewGoal</div>
-  )
+  const exercise = useLoaderData();
+  return <div className="container">
+      <h1>New goal for {exercise.title}</h1>
+      <Form method="POST">
+          <div className="form-control">
+            <label htmlFor="weight">weight</label>
+            <input type="number" name="weight" id="weight" />
+          </div>
+            {/* <div className="error">
+              <p>
+                {actionData?.fieldErrors?.reps && actionData?.fieldErrors?.reps}
+              </p>
+            </div> */}
+          <button type="submit" className="btn btn-block">
+            Add goal
+          </button>
+        </Form>
+  </div>;
 }

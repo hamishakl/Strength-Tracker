c53f17462a2869caf613d208a9004f8baffd27f2
Hamish Henare <hamishjustin7@gmail.com>
2022-02-14 11:55:29 +1300

    goal progress only renders if goal set

--- app/components/Goals.jsx
+++ app/components/Goals.jsx
@@ -16,43 +16,48 @@ const calcProgress = (exercise, latestPr) => {
   const current = latestPr.weight;
   const percentage = (current / goal) * 100;
   if (exercise.goal == null) {
-    return 0
+    return 0;
   } else {
-    console.log(percentage.toFixed(2))
-    return percentage.toFixed(2)
+    return percentage.toFixed(2);
   }
 };
 
 export default function Recommendation({ exercise, prs }) {
   const latestPr = findPr(prs, exercise);
-  let percentage 
-  latestPr === null ? percentage = 0 : percentage = calcProgress(exercise, latestPr)
-  
+  let percentage;
+  latestPr === null
+    ? (percentage = 0)
+    : (percentage = calcProgress(exercise, latestPr));
 
   return (
-    <div className="container">
-      <div className="card" style={{ flex: "0 0 33.333333%" }}>
-        <div className="card-body">
-          <h6 className="card-subtitle mb-2 text-muted"></h6>
-          {exercise.goal === null ? (
-            <p>no goal</p>
-          ) : (
-            <p>Progress towards your goal of {exercise.goal}kg</p>
-          )}
-          <div class="progress">
-            <div
-              class="progress-bar"
-              role="progressbar"
-              style={{ width: `${percentage}%` }}
-              aria-valuenow="25"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            >
-              {percentage}%
+    <>
+      {exercise.goal === null ? (
+        <p>No goal set</p>
+      ) : (
+        <div className="container">
+          <div className="card" style={{ flex: "0 0 33.333333%" }}>
+            <div className="card-body">
+              {exercise.goal === null ? (
+                <p>no goal</p>
+              ) : (
+                <p>Progress towards your goal of {exercise.goal}kg</p>
+              )}
+              <div className="progress">
+                <div
+                  className="progress-bar"
+                  role="progressbar"
+                  style={{ width: `${percentage}%` }}
+                  aria-valuenow="25"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                >
+                  {percentage}%
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }

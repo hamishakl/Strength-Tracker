1615701fb1d47d2369a0ed40f2e4aaf230fb9227
Hamish Henare <hamishjustin7@gmail.com>
2022-03-10 11:48:34 +1300

    fixed no pr set issue

--- app/components/MyExercises.jsx
+++ app/components/MyExercises.jsx
@@ -1,36 +1,41 @@
-import { Link } from "remix";
-import Goals from "./Goals";
+import { Link } from 'remix';
+import Goals from './Goals';
 
 export default function MyExercise({ data }) {
-  const exercises = data.exercises["exercises"];
-  const prs = data.prs["prs"];
+  const exercises = data.exercises['exercises'];
+  const prs = data.prs['prs'];
   return (
-    <div className="container">
-      <div className="row">
+    <div className='container'>
+      <div className='row'>
         {exercises.map((exercise) => (
           <div
             key={exercise.id}
-            className="card"
-            style={{ flex: "0 0 33.333333%" }}
+            className='card'
+            style={{ flex: '0 0 33.333333%' }}
           >
-            <div className="card-body" key={exercise.id}>
+            <div className='card-body' key={exercise.id}>
               <Link to={exercise.id}>
-                <h5 className="card-title">{exercise.title}</h5>
+                <h5 className='card-title'>{exercise.title}</h5>
               </Link>
-              <h6 className="card-subtitle mb-2 text-muted"></h6>
-              <p className="card-text">{exercise.body}</p>
-              <ul className="nav">
-                <li className="nav-item">
+              <h6 className='card-subtitle mb-2 text-muted'></h6>
+              <p className='card-text'>{exercise.body}</p>
+              <ul className='nav'>
+                <li className='nav-item'>
                   <Link to={`./${exercise.id}/pr-new`}>New PR</Link>
                 </li>
-                <li className="nav-item">
+                <li className='nav-item'>
                   <Link to={`${exercise.id}/new-goal`}>New goal</Link>
                 </li>
               </ul>
-              {
-                prs.length === 0 ? (<br></br>) : 
-                <Goals exercise={exercise} prs={prs} />
-              }
+              {prs.length === 0 ? (
+                <br></br>
+              ) : (
+                prs.map((pr) => {
+                  <div key={pr.id}>
+                    <Goals exercise={exercise} prs={pr} />
+                  </div>;
+                })
+              )}
             </div>
           </div>
         ))}

8043619db530978eb91e236f2bfd20f1a765f4d5
hamish <hamishjustin7@gmail.com>
2022-05-07 19:08:38 +1200

    workouts now render on dashboard

--- app/routes/dashboard/index.jsx
+++ app/routes/dashboard/index.jsx
@@ -1,12 +1,12 @@
-import { Link, useLoaderData } from 'remix';
-import { db } from '~/utils/db.server';
-import MyExercise from '../../components/MyExercises';
+import { Link, useLoaderData } from 'remix'
+import { db } from '~/utils/db.server'
+import MyExercise from '../../components/MyExercises'
 
-import { getUser } from '~/utils/session.server';
-import MyWorkouts from '~/components/MyWorkouts';
+import { getUser } from '~/utils/session.server'
+import MyWorkouts from '~/components/MyWorkouts'
 
 export const loader = async ({ request }) => {
-  const user = await getUser(request);
+  const user = await getUser(request)
 
   const exercises = {
     exercises: await db.exercise.findMany({
@@ -18,7 +18,7 @@ export const loader = async ({ request }) => {
       take: 20,
       orderBy: { createdAt: 'desc' },
     }),
-  };
+  }
   const prs = {
     prs: await db.pr.findMany({
       where: {
@@ -28,7 +28,7 @@ export const loader = async ({ request }) => {
       },
       orderBy: { createdAt: 'desc' },
     }),
-  };
+  }
   const workouts = {
     workouts: await db.workout.findMany({
       where: {
@@ -40,45 +40,47 @@ export const loader = async ({ request }) => {
         date: true,
       },
       include: {
-            volume: {
-              select: {
-                exerciseId: true,
-                weight: true,
-                reps: true, 
-                sets: true,
-                workoutId: true,
-                id: true,
-              }
+        volume: {
+          select: {
+            exerciseId: true,
+            weight: true,
+            reps: true,
+            sets: true,
+            workoutId: true,
+            id: true,
           },
         },
-        orderBy: { createdAt: 'desc' },
       },
-        ),
-  };
+      orderBy: { createdAt: 'desc' },
+    }),
+  }
 
-  
-  const data = { exercises, prs, workouts};
+  const data = { exercises, prs, workouts }
 
-  return data;
-};
+  return data
+}
 
 function ExerciseItems() {
-  const data = useLoaderData();
+  const data = useLoaderData()
   const workoutData = data.workouts['workouts']
   return (
-    <div className='h-screen w-screen max-w-none max-h-none bg-white p-0 m-0 text-black'>
+    <div className='max-h-none m-0 h-screen w-screen max-w-none bg-white p-0 text-black'>
       <div className='flex w-screen items-center justify-between'>
         <h1 className='text-3xl font-bold'>My Exercises</h1>
         <Link to='/dashboard/new' className='underline-offset-4'>
-          <span className='underline-offset-4 underline opacity-80 hover:opacity-100 ease-linear duration-100 hover:underline-offset-2'>New Exercise</span>
+          <span className='underline underline-offset-4 opacity-80 duration-100 ease-linear hover:underline-offset-2 hover:opacity-100'>
+            New Exercise
+          </span>
         </Link>
       </div>
-      <MyExercise exercises={data.exercises['exercises']} prs={data.prs['prs']} />
-        <MyWorkouts data={workoutData}/>
-      <div>
-      </div>
+      <MyExercise
+        exercises={data.exercises['exercises']}
+        prs={data.prs['prs']}
+      />
+      <MyWorkouts data={workoutData} />
+      <div></div>
     </div>
-  );
+  )
 }
 
-export default ExerciseItems;
+export default ExerciseItems

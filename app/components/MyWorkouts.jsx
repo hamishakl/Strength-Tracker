ad0982156cbc68da9b40f6dbd4c3270166147d71
Hamish Henare <60582221+hamishakl@users.noreply.github.com>
2022-05-10 12:57:27 +1200

    duplicate workout exercises are only rendered once

--- app/components/MyWorkouts.jsx
+++ app/components/MyWorkouts.jsx
@@ -1,40 +1,66 @@
 export default function MyWorkouts({ data }) {
-  // console.log(data)
   return (
     <>
       <h2 className="subheading">Workouts</h2>
-    <div className="workout-wrapper">
-      {data.reduce((acc, current) => {
-        // acc.push(current['Exercise'].title)
-        console.log(current.volume)
-      }, [])}
-      {data.map((workouts) => {
-        let dateStr = new Date(`${workouts.date}`);
-        let dateArr = dateStr.toDateString().split(' ');
-        let date = dateArr[2] + ' ' + dateArr[1] + ' ' + dateArr[3];
-        
+      <div className="workout-wrapper">
+        {data.map((workouts, index) => {
+          let dateStr = new Date(`${workouts.date}`);
+          let dateArr = dateStr.toDateString().split(' ');
+          let date = dateArr[2] + ' ' + dateArr[1] + ' ' + dateArr[3];
 
-        return (
-          <div className="workout-card">
-            <h2>{date}</h2>
-            
-            
-            {workouts.volume.map((workout, index) => {
-             
-              return (
-                <div>
-                  <h4>{workout['Exercise'].title}</h4>
-                  <p>
-                    {workout.weight}kg for {workout.reps} reps for{' '}
-                    {workout.sets} sets.
-                  </p>
-                </div>
-              )
-            })}
-          </div>
-        )
-      })}
-    </div>
-      </>
+          console.log('look here')
+
+          return (
+            <div className="workout-card" key={index}>
+              <h2>{date}</h2>
+              {workouts.volume.map((workout, index, arr) => {
+                console.log(workout)
+                index++
+                let previousArrTitle = arr[index - 1]
+                let arrTitle = arr[index]
+                return (
+                  <div key={index}>
+                    {
+                      arrTitle != undefined && previousArrTitle != undefined ?
+
+                        (
+                          arrTitle['Exercise'].title === previousArrTitle['Exercise'].title ?
+                            (
+                              <>
+                                <h4>{previousArrTitle['Exercise'].title}</h4>
+                                <p>
+                                  {previousArrTitle.weight}kg for {previousArrTitle.reps} reps for{' '}
+                                  {previousArrTitle.sets} sets.
+                                </p>
+                                <p>
+                                  {arrTitle.weight}kg for {arrTitle.reps} reps for{' '}
+                                  {arrTitle.sets} sets.
+                                </p>
+                              </>
+                            )
+                            :
+                            (
+                              <>
+                                <h4>{arrTitle['Exercise'].title}</h4>
+                                <p>
+                                  {arrTitle.weight}kg for {arrTitle.reps} reps for{' '}
+                                  {arrTitle.sets} sets.
+                                </p>
+                              </>
+                            )
+                        )
+                        :
+                        (
+                          console.log()
+                        )
+                    }
+                  </div>
+                )
+              })}
+            </div>
+          )
+        })}
+      </div>
+    </>
   )
 }

10ab53b49622c1d55cb4594c3f98760a87bc1422
hamish <hamishjustin7@gmail.com>
2022-05-15 09:37:05 +1200

    user is redirected to dashboard when opening website if currently still logged in

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,19 +1,31 @@
-import styles from "~/styles/home.css";
+import styles from '~/styles/home.css'
+import { redirect, useLoaderData } from 'remix'
+import { getUser } from '../utils/session.server'
 
 export function links() {
-  return [{ rel: "stylesheet", href: styles }];
+  return [{ rel: 'stylesheet', href: styles }]
+}
+
+export const loader = async ({ request }) => {
+  const user = await getUser(request)
+  const data = {
+    user,
+  }
+
+  if (user) {
+    return redirect('/dashboard')
+  }
+
+  return data
 }
 
 function Home() {
-
-
-
   return (
-    <div className="">
+    <div className=''>
       <h1>Strength Tracker</h1>
       <p>Track your personal records with ease</p>
     </div>
   )
 }
-    
-export default Home;
+
+export default Home

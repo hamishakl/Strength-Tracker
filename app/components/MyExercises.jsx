a212b2c8a0a85a105970b671cd234fc3a15dd4aa
hamish <hamishjustin7@gmail.com>
2022-05-19 09:50:46 +1200

    tidied up code - deleted unwanted comments and console logs

--- app/components/MyExercises.jsx
+++ app/components/MyExercises.jsx
@@ -1,41 +1,38 @@
 import { Link } from 'remix'
-// import Goals from './Goals'
-// import { OneRmEstimate } from '../routes/dashboard/$exerciseId'
 
 export const OneRmEstimate = (weight, reps) => {
   const unRounded1RM = weight * reps * 0.0333 + weight
   return reps === 1 ? weight : Math.round(unRounded1RM / 2.5, 1) * 2.5
 }
 
-export default function MyExercise({ exercises, prs }) {
+export default function MyExercise({ exercises }) {
   return (
     <div className='exercise-list__div'>
-      {exercises.map((exercise, index) => {
-          let dateSplit = exercise.updatedAt.split("")
-          let dateStr = dateSplit.slice(0, 10)
-          let newDate = new Date(`${dateStr.join('')}`);
-          let dateArr = newDate.toDateString().split(' ');
-          let date = dateArr[2] + ' ' + dateArr[1] + ' ' + dateArr[3];
-
+      {exercises.map((exercise) => {
+        let dateSplit = exercise.updatedAt.split('')
+        let dateStr = dateSplit.slice(0, 10)
+        let newDate = new Date(`${dateStr.join('')}`)
+        let dateArr = newDate.toDateString().split(' ')
+        let date = dateArr[2] + ' ' + dateArr[1] + ' ' + dateArr[3]
 
         let oneRm
         let weight
         let reps
-        exercise['Pr'].length === 0 ? oneRm = null : (
-         weight = exercise['Pr'][0]['weight'],
-         reps = exercise['Pr'][0]['reps'],
-          oneRm = OneRmEstimate(weight, reps)
-        )
-        
+        exercise['Pr'].length === 0
+          ? (oneRm = null)
+          : ((weight = exercise['Pr'][0]['weight']),
+            (reps = exercise['Pr'][0]['reps']),
+            (oneRm = OneRmEstimate(weight, reps)))
+
         return (
           <>
-            <div class="transfer" key={exercise.id}>
-              <dl class="transfer-details">
+            <div class='transfer' key={exercise.id}>
+              <dl class='transfer-details'>
                 <div>
                   <Link to={`/dashboard/${exercise.id}`}>{exercise.title}</Link>
                 </div>
                 <div>
-                  <dt>{oneRm === null ? ('No PR') : oneRm + 'kg'}</dt>
+                  <dt>{oneRm === null ? 'No PR' : oneRm + 'kg'}</dt>
                   <dd>Best 1RM Estimate</dd>
                 </div>
                 <div>
@@ -46,7 +43,6 @@ export default function MyExercise({ exercises, prs }) {
             </div>
           </>
         )
-
       })}
     </div>
   )
--- app/routes/dashboard/$exerciseId.volume.jsx
+++ app/routes/dashboard/$exerciseId.volume.jsx
@@ -1,34 +1,27 @@
-import {
-  Link,
-  redirect,
-  useActionData,
-  json,
-  Form,
-  useLoaderData,
-} from 'remix';
-import { db } from '~/utils/db.server';
-import { getUser } from '~/utils/session.server';
+import { Link, redirect, useActionData, json, Form, useLoaderData } from 'remix'
+import { db } from '~/utils/db.server'
+import { getUser } from '~/utils/session.server'
 
 function validateWeight(weight) {
   if (typeof weight !== 'number') {
-    return 'weight should be atleast 2 characters long';
+    return 'weight should be atleast 2 characters long'
   }
 }
 
 function validateReps(reps) {
   if (typeof reps !== 'number') {
-    return 'reps should be atleast 2 characters long';
+    return 'reps should be atleast 2 characters long'
   }
 }
 
 function validateReps(sets) {
   if (typeof sets !== 'number') {
-    return 'reps should be atleast 2 characters long';
+    return 'reps should be atleast 2 characters long'
   }
 }
 
 function badRequest(data) {
-  return json(data, { status: 400 });
+  return json(data, { status: 400 })
 }
 
 export const loader = async ({ params }) => {
@@ -36,44 +29,44 @@ export const loader = async ({ params }) => {
     where: {
       id: params.exerciseId,
     },
-  });
-  return exercise;
-};
+  })
+  return exercise
+}
 
 export const action = async ({ request, params }) => {
-  const form = await request.formData();
-  const weightStr = form.get('weight');
-  const repsStr = form.get('reps');
-  const setsStr = form.get('sets');
-  const weight = parseInt(weightStr);
-  const sets = parseInt(setsStr);
-  const reps = parseInt(repsStr);
-  const exerciseId = params.exerciseId;
+  const form = await request.formData()
+  const weightStr = form.get('weight')
+  const repsStr = form.get('reps')
+  const setsStr = form.get('sets')
+  const weight = parseInt(weightStr)
+  const sets = parseInt(setsStr)
+  const reps = parseInt(repsStr)
+  const exerciseId = params.exerciseId
 
-  const user = await getUser(request);
+  const user = await getUser(request)
 
-  const fields = { weight, reps, sets };
+  const fields = { weight, reps, sets }
 
   const fieldErrors = {
     weight: validateWeight(weight),
     reps: validateReps(reps),
-  };
+  }
 
   if (Object.values(fieldErrors).some(Boolean)) {
-    console.log(fieldErrors);
-    return badRequest({ fieldErrors, fields });
+    console.log(fieldErrors)
+    return badRequest({ fieldErrors, fields })
   }
 
   const volume = await db.volume.create({
     data: { ...fields, userId: user.id, exerciseId: exerciseId },
-  });
+  })
 
-  return redirect(`/dashboard/${exerciseId}`);
-};
+  return redirect(`/dashboard/${exerciseId}`)
+}
 
 function NewPr() {
-  const actionData = useActionData();
-  const exercise = useLoaderData();
+  const actionData = useActionData()
+  const exercise = useLoaderData()
 
   return (
     <>
@@ -119,7 +112,7 @@ function NewPr() {
         </Form>
       </div>
     </>
-  );
+  )
 }
 
-export default NewPr;
+export default NewPr

cf49a01f4cf62cb9ce9060230fe9c28f3d52b5ed
Hamish Henare <60582221+hamishakl@users.noreply.github.com>
2022-05-20 17:33:13 +1200

    made a start on homepage - home navbar and hero co,pleted

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,10 +1,23 @@
 import { redirect, useLoaderData } from 'remix'
 import { getUser } from '../utils/session.server'
+import WorkoutIcon from '../../public/images/hero-img.jpeg'
 
-export function links() {
-  return [{ rel: 'stylesheet', href: styles }]
+
+import cssSheet from '~/styles/app.css'
+import homepageCssSheet from '~/styles/homepage.css'
+
+export const links = () => {
+  return [
+    {
+      rel: 'stylesheet',
+      href: cssSheet,
+    },
+    {
+      rel: 'stylesheet',
+      href: homepageCssSheet,
+    },
+  ]
 }
-
 export const loader = async ({ request }) => {
   const user = await getUser(request)
   const data = {
@@ -20,9 +33,24 @@ export const loader = async ({ request }) => {
 
 function Home() {
   return (
-    <div className=''>
-      <h1>Strength Tracker</h1>
-      <p>Track your personal records with ease</p>
+    <div className='layout'>
+      <container className='hero'>
+        <div className='hero-contents__div'>
+          <h1 className='hero__logo'>Measure progress</h1>
+          <p className='hero__copy'>Stay on track with your fitness goals. Create a profile and track your strength progress to meet individual goals and challenges.</p>
+          <div className='hero-buttons__wrapper'>
+            <button className='hero__buttons register'>Register</button>
+            <button className='hero__buttons login'>Login</button>
+          </div>
+        </div>
+        <img src={WorkoutIcon} alt="" />
+      </container>
+      <container className='intro'>
+          <p id='test'>
+            Log your workouts and reach new PRs with Strength Tracker. With just a few clicks, track personal records from past workouts, set goals for future ones, monitor exercise progress, explore new exercises—all on one screen.
+          </p>
+            <img src="" alt="" />
+      </container>
     </div>
   )
 }

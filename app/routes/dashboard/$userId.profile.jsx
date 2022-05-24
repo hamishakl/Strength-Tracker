a9437c2fb5604008133a9f70f00e94204debe45f
hamish <hamishjustin7@gmail.com>
2022-05-24 18:45:27 +1200

    switching pc

--- app/routes/dashboard/$userId.profile.jsx
+++ app/routes/dashboard/$userId.profile.jsx
@@ -1,174 +1,227 @@
-import React from 'react'
-import { useLoaderData, json, useActionData } from 'remix'
-import { getUser } from '~/utils/session.server'
-import { useState } from 'react'
-import { db } from '~/utils/db.server'
+import React from "react";
+import { useLoaderData, json, useActionData } from "remix";
+import { getUser } from "~/utils/session.server";
+import { useState } from "react";
+import { db } from "~/utils/db.server";
+import bcrypt from "bcrypt";
+
+import { changePassword } from "~/utils/session.server";
 
 function badRequest(data) {
-  return json(data, { status: 400 })
+  return json(data, { status: 400 });
 }
 
 export const loader = async ({ request }) => {
-  const user = await getUser(request)
-  return user
-}
+  const user = await getUser(request);
+  return user;
+};
 
 export const action = async ({ request, params }) => {
-  const form = await request.formData()
-  const name = form.get('name')
+  const form = await request.formData();
+  const name = form.get("name");
 
-  if (form.get('_method') === 'name') {
-    const user = await getUser(request)
+  if (form.get("_method") === "name") {
+    const user = await getUser(request);
 
     await db.user.update({
       where: { id: user.id },
       data: {
         name: name,
       },
-    })
+    });
 
-    return null
+    return null;
   }
-  if (form.get('_method') === 'email') {
-    const user = await getUser(request)
-    const email = form.get('email')
-    const fields = { email }
+  if (form.get("_method") === "email") {
+    const user = await getUser(request);
+    const email = form.get("email");
+    const fields = { email };
     const emailExists = await db.user.findFirst({
       where: {
         email,
       },
-    })
+    });
     if (emailExists) {
       return badRequest({
         fields,
-        fieldErrors: {email: `the email address: ${email} already exists.`},
-      })
+        fieldErrors: { email: `the email address: ${email} already exists.` },
+      });
     }
-  
 
     await db.user.update({
       where: { id: user.id },
       data: {
         email: email,
       },
-    })
+    });
 
-    return null
+    if (form.get("_method") === "password") {
+      const password = form.get("");
+      const passwordHash = await bcrypt.hash(password, 10);
+
+      const passwordTrue = await changePassword({ email, password });
+      if (!passwordTrue) {
+        return badRequest({
+          fields,
+          fieldErrors: { password: "Invalid Credentials" },
+        });
+      }
+      // return createUserSession(user.id, "/dashboard");
+
+      await db.user.update({
+        where: { id: user.id },
+        data: {
+          passwordHash: passwordHash,
+        },
+      });
+    }
+
+    return null;
   }
 
-  let data = form.get('_rename')
+  let data = form.get("_rename");
   if (data != undefined) {
     const exercise = await db.exercise.findUnique({
       where: { id: params.exerciseId },
-    })
+    });
 
     await db.exercise.update({
       where: { id: params.exerciseId },
       data: {
         title: data,
       },
-    })
+    });
   }
 
-  return redirect(`/dashboard/${params.exerciseId}`)
-}
+  return null;
+};
 
 export default function register() {
-  const actionData = useActionData()
+  const actionData = useActionData();
 
-  let [count, setPage] = useState(0)
+  let [count, setPage] = useState(0);
 
-  const user = useLoaderData()
-  console.log(user)
+  const user = useLoaderData();
+  console.log(user);
   return (
     <div>
       {count === 1 && (
         <>
-        <div>
-          <form method='POST' className=''>
-            <input type='hidden' name='_method' value='name' />
-            <input
-              className=''
-              name='name'
-              type='text'
-              placeholder={user.name}
-              />
-            <button type='submit' className=''>
-              Save
-            </button>
-          </form>
-        </div>
-        <div>
-          <h1 className='' onClick={() => setPage((count = 2))}>
-            {user.email}
-          </h1>
-          <a className='menu-item' onClick={() => setPage((count = 2))}>
-            Rename
-          </a>
-        </div>
-              </>
-      )}
-      {count === 0 && (
-        <div>
-          <h1 className='' onClick={() => setPage((count = 1))}>
-            {user.name}
-          </h1>
-          <a className='menu-item' onClick={() => setPage((count = 1))}>
-            Rename
-          </a>
-        </div>
-      )}
-      {count === 2 && (
-        <>
           <div>
-            <h1 className='' onClick={() => setPage((count = 1))}>
-              {user.name}
-            </h1>
-            <a className='menu-item' onClick={() => setPage((count = 1))}>
-              Rename
-            </a>
-          </div>
-          <div>
-            <form method='POST' className=''>
-              <input type='hidden' name='_method' value='email' />
+            <form method="POST" className="">
+              <input type="hidden" name="_method" value="name" />
               <input
-                className=''
-                name='email'
-                type='text'
-                placeholder={user.email}
+                className=""
+                name="name"
+                type="text"
+                placeholder={user.name}
+                onBlur={console.log("blurrr")}
               />
-              <button type='submit' className=''>
+              <button type="submit" className="">
                 Save
               </button>
             </form>
           </div>
+          <div>
+            <h1 className="" onClick={() => setPage((count = 2))}>
+              {user.email}
+            </h1>
+            <a className="menu-item" onClick={() => setPage((count = 2))}>
+              Edit email
+            </a>
+          </div>
+          <button onClick={() => setPage((count = 3))}>Change password</button>
         </>
       )}
-      <div className='error'>
-          {actionData?.fieldErrors?.email &&
-            actionData?.fieldErrors?.email}
-        </div>
+
       {count === 0 && (
-        <div>
-          <h1 className='' onClick={() => setPage((count = 2))}>
-            {user.email}
-          </h1>
-          <a className='menu-item' onClick={() => setPage((count = 2))}>
-            Rename
-          </a>
-        </div>
+        <>
+          <div>
+            <h1 className="" onClick={() => setPage((count = 1))}>
+              {user.name}
+            </h1>
+            <a className="menu-item" onClick={() => setPage((count = 1))}>
+              Edit name
+            </a>
+          </div>
+          <div>
+            <h1 className="" onClick={() => setPage((count = 2))}>
+              {user.email}
+            </h1>
+            <a className="menu-item" onClick={() => setPage((count = 2))}>
+              Edit email
+            </a>
+          </div>
+          <button onClick={() => setPage((count = 3))}>Change password</button>
+        </>
       )}
-      <form action="">
-        <a>Change password</a>
-        <label htmlFor="old-password">Old password</label>
-        <input type="password" name='old-password' />
-        <label htmlFor="new-password">New password</label>
-        <input type="password" name='new-password' />
-        <label htmlFor="confirm-new-password">Confirm new password</label>
-        <input type="password" name='confirm-new-password' />
-      </form>
+
+      {count === 2 && (
+        <>
+          <div>
+            <h1 className="" onClick={() => setPage((count = 1))}>
+              {user.name}
+            </h1>
+            <a className="menu-item" onClick={() => setPage((count = 1))}>
+              Edit name
+            </a>
+          </div>
+          <div>
+            <form method="POST" className="">
+              <input type="hidden" name="_method" value="email" />
+              <input
+                className=""
+                name="email"
+                type="text"
+                placeholder={user.email}
+                onBlur={console.log("blurrremail")}
+              />
+              <div className="error">
+                {actionData?.fieldErrors?.email &&
+                  actionData?.fieldErrors?.email}
+              </div>
+              <button type="submit" className="">
+                Save
+              </button>
+            </form>
+          </div>
+          <button onClick={() => setPage((count = 3))}>Change password</button>
+        </>
+      )}
+
+      {count === 3 && (
+        <>
+          <div>
+            <h1 className="" onClick={() => setPage((count = 1))}>
+              {user.name}
+            </h1>
+            <a className="menu-item" onClick={() => setPage((count = 1))}>
+              Edit name
+            </a>
+          </div>
+          <div>
+            <h1 className="" onClick={() => setPage((count = 2))}>
+              {user.email}
+            </h1>
+            <a className="menu-item" onClick={() => setPage((count = 2))}>
+              Edit email
+            </a>
+          </div>
+          <form action="POST">
+            <input type="hidden" name="_method" value="password" />
+            <label htmlFor="old-password">Old password</label>
+            <div className="error">
+              {actionData?.fieldErrors?.password && actionData?.fieldErrors?.password}
+            </div>
+            <input type="password" name="old-password" />
+            <label htmlFor="new-password">New password</label>
+            <input type="password" name="new-password" />
+            <label htmlFor="confirm-new-password">Confirm new password</label>
+            <input type="password" name="confirm-new-password" />
+            <button type="submit">Submit</button>
+          </form>
+        </>
+      )}
     </div>
-  )
+  );
 }
-
-
--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -1,100 +1,112 @@
-import bcrypt from 'bcrypt'
-import { db } from './db.server'
-import { createCookieSessionStorage, redirect } from 'remix'
+import bcrypt from "bcrypt";
+import { db } from "./db.server";
+import { createCookieSessionStorage, redirect } from "remix";
 
 
 export async function login({ email, password }) {
-    const user = await db.user.findUnique({
-        where: {
-            email
-        }
-    })
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
 
-    if (!user) {
-        return null
-    }
+  if (!user) {
+    return null;
+  }
 
-    const isCorrectPassword = await bcrypt.compare(password, user.passwordHash)
+  const isCorrectPassword = await bcrypt.compare(password, user.passwordHash);
 
-    if (!isCorrectPassword) return null
+  if (!isCorrectPassword) return null;
 
-    return user
+  return user;
+}
+
+export async function changePassword({ email, password }) {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    return null;
+  }
+
+  const isCorrectPassword = await bcrypt.compare(password, user.passwordHash);
+
+  if (!isCorrectPassword) return null;
+
+  return true;
 }
 
 export async function register({ email, password, name }) {
-    const passwordHash = await bcrypt.hash(password, 10)
-    return db.user.create({
-        data: {
-            name,
-            email,
-            passwordHash
-        }
-    })
+  const passwordHash = await bcrypt.hash(password, 10);
+  return db.user.create({
+    data: {
+      name,
+      email,
+      passwordHash,
+    },
+  });
 }
 
-const sessionSecret = process.env.SESSION_SECRET
+const sessionSecret = process.env.SESSION_SECRET;
 if (!sessionSecret) {
-    throw new Error('No session secret')
+  throw new Error("No session secret");
 }
 
 const storage = createCookieSessionStorage({
-    cookie: {
-        name: 'remixblog_session',
-        secure: process.env.NODE_ENV === 'production',
-        secrets: [sessionSecret],
-        sameSite: 'lax',
-        path: '/',
-        maxAge: 60 * 60 * 24 * 60,
-        httpOnly: true
-    }
-})
+  cookie: {
+    name: "remixblog_session",
+    secure: process.env.NODE_ENV === "production",
+    secrets: [sessionSecret],
+    sameSite: "lax",
+    path: "/",
+    maxAge: 60 * 60 * 24 * 60,
+    httpOnly: true,
+  },
+});
 
 export async function createUserSession(userId: string, redirectTo: string) {
-
-    console.log('redirect');
-    const session = await storage.getSession()
-    session.set(
-        'userId', userId
-    )
-    return redirect(
-
-        redirectTo, {
-        headers: {
-            'Set-Cookie': await storage.commitSession(session)
-        }
-    }
-    )
+  console.log("redirect");
+  const session = await storage.getSession();
+  session.set("userId", userId);
+  return redirect(redirectTo, {
+    headers: {
+      "Set-Cookie": await storage.commitSession(session),
+    },
+  });
 }
 
 export function getUserSession(request: Request) {
-    return storage.getSession(request.headers.get('Cookie'))
+  return storage.getSession(request.headers.get("Cookie"));
 }
 
 export async function getUser(request: Request) {
-    const session = await getUserSession(request)
-    const userId = session.get('userId')
-    if (!userId || typeof userId !== 'string') {
-        return null
-    }
+  const session = await getUserSession(request);
+  const userId = session.get("userId");
+  if (!userId || typeof userId !== "string") {
+    return null;
+  }
 
-    try {
-        const user = await db.user.findUnique({
-            where: {
-                id: userId
-            }
-        })
-        return user
-    } catch (error) {
-        return null
-    }
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    return user;
+  } catch (error) {
+    return null;
+  }
 }
 
 export async function logout(request: Request) {
-    const session = await storage.getSession(request.headers.get('Cookie'))
+  const session = await storage.getSession(request.headers.get("Cookie"));
 
-    return redirect('/auth/logout', {
-        headers: {
-            'Set-Cookie': await storage.destroySession(session)
-        }
-    })
+  return redirect("/auth/logout", {
+    headers: {
+      "Set-Cookie": await storage.destroySession(session),
+    },
+  });
 }

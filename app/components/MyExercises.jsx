40074c9a48069d603e52efcf89c5f67c3a32f7bf
Hamish Henare <hamishjustin7@gmail.com>
2022-05-26 16:03:42 +1200

    created goals index that includes current and past goals, also add new goal component

--- app/components/MyExercises.jsx
+++ app/components/MyExercises.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'remix'
+import { Link } from "remix"
 
 export const OneRmEstimate = (weight, reps) => {
   const unRounded1RM = weight * reps * 0.0333 + weight
@@ -9,37 +9,39 @@ export default function MyExercise({ exercises }) {
   return (
     <div className='exercise-list__div'>
       {exercises.map((exercise, index) => {
-        let dateSplit = exercise.updatedAt.split('')
+        let dateSplit = exercise.updatedAt.split("")
         let dateStr = dateSplit.slice(0, 10)
-        let newDate = new Date(`${dateStr.join('')}`)
-        let dateArr = newDate.toDateString().split(' ')
-        let date = dateArr[2] + ' ' + dateArr[1] + ' ' + dateArr[3]
+        let newDate = new Date(`${dateStr.join("")}`)
+        let dateArr = newDate.toDateString().split(" ")
+        let date = dateArr[2] + " " + dateArr[1] + " " + dateArr[3]
 
         let oneRm
         let weight
         let reps
-        exercise['Pr'].length === 0
+        exercise["Pr"].length === 0
           ? (oneRm = null)
-          : ((weight = exercise['Pr'][0]['weight']),
-            (reps = exercise['Pr'][0]['reps']),
+          : ((weight = exercise["Pr"][0]["weight"]),
+            (reps = exercise["Pr"][0]["reps"]),
             (oneRm = OneRmEstimate(weight, reps)))
 
         return (
-            <div className={'transfer'} key={exercise.id}>
-              <dl className={'transfer-details'}>
-                <div>
-                  <Link to={`/dashboard/exercises/${exercise.id}`}>{exercise.title}</Link>
-                </div>
-                <div>
-                  <dt>{oneRm === null ? 'No PR' : oneRm + 'kg'}</dt>
-                  <dd>Best 1RM Estimate</dd>
-                </div>
-                <div>
-                  <dt>{date}</dt>
-                  <dd>Last updated</dd>
-                </div>
-              </dl>
-            </div>
+          <div className={"transfer"} key={exercise.id}>
+            <dl className={"transfer-details"}>
+              <div>
+                <Link to={`/dashboard/exercises/${exercise.id}`}>
+                  {exercise.title}
+                </Link>
+              </div>
+              <div>
+                <dt>{oneRm === null ? "No PR" : oneRm + "kg"}</dt>
+                <dd>Best 1RM Estimate</dd>
+              </div>
+              <div>
+                <dt>{date}</dt>
+                <dd>Last updated</dd>
+              </div>
+            </dl>
+          </div>
         )
       })}
     </div>
--- app/routes/dashboard/exercises/$exerciseId.jsx
+++ app/routes/dashboard/exercises/$exerciseId.jsx
@@ -1,17 +1,17 @@
-import { Link, redirect, useLoaderData } from 'remix'
-import { db } from '~/utils/db.server'
-import { getUser } from '~/utils/session.server'
-import PrTable from '../../../components/PrTable'
-import Chart from '../../../components/Chart'
-import { useState } from 'react'
+import { Link, redirect, useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+import { getUser } from "~/utils/session.server";
+import PrTable from "../../../components/PrTable";
+import Chart from "../../../components/Chart";
+import { useState } from "react";
 
 export const loader = async ({ request, params }) => {
-  const user = await getUser(request)
+  const user = await getUser(request);
   const exercise = await db.exercise.findUnique({
     where: { id: params.exerciseId },
-  })
+  });
 
-  if (!exercise) throw new Error('exercise not found')
+  if (!exercise) throw new Error("exercise not found");
   const pr = await db.pr.findMany({
     where: {
       userId: {
@@ -22,88 +22,88 @@ export const loader = async ({ request, params }) => {
       },
     },
     orderBy: {
-      createdAt: 'desc',
+      createdAt: "desc",
     },
-  })
-  const oneRepMax = weightLoop(pr)
-  const data = { exercise, user, pr, oneRepMax }
-  return data
-}
+  });
+  const oneRepMax = weightLoop(pr);
+  const data = { exercise, user, pr, oneRepMax };
+  return data;
+};
 
 const weightLoop = (pr) => {
-  let arr = []
+  let arr = [];
   for (let i = 0; i < pr.length; i++) {
-    arr.push(OneRmEstimate(pr[i].weight, pr[i].reps))
+    arr.push(OneRmEstimate(pr[i].weight, pr[i].reps));
   }
-  return Math.max(...arr)
-}
+  return Math.max(...arr);
+};
 
 export const action = async ({ request, params }) => {
-  const form = await request.formData()
-  if (form.get('_method') === 'delete') {
-    const user = await getUser(request)
+  const form = await request.formData();
+  if (form.get("_method") === "delete") {
+    const user = await getUser(request);
 
     const exercise = await db.exercise.findUnique({
       where: { id: params.exerciseId },
-    })
+    });
 
-    if (!exercise) throw new Error('exercise not found')
+    if (!exercise) throw new Error("exercise not found");
 
     if (user && exercise.userId === user.id) {
-      await db.exercise.delete({ where: { id: params.exerciseId } })
+      await db.exercise.delete({ where: { id: params.exerciseId } });
     }
 
-    return redirect('/dashboard')
+    return redirect("/dashboard");
   }
-  let data = form.get('_rename')
+  let data = form.get("_rename");
   if (data != undefined) {
     const exercise = await db.exercise.findUnique({
       where: { id: params.exerciseId },
-    })
+    });
 
     await db.exercise.update({
       where: { id: params.exerciseId },
       data: {
         title: data,
       },
-    })
+    });
   }
 
-  return redirect(`/dashboard/${params.exerciseId}`)
-}
+  return redirect(`/dashboard/${params.exerciseId}`);
+};
 
 export const OneRmEstimate = (weight, reps) => {
-  const unRounded1RM = weight * reps * 0.0333 + weight
-  return reps === 1 ? weight : Math.round(unRounded1RM / 2.5, 1) * 2.5
-}
+  const unRounded1RM = weight * reps * 0.0333 + weight;
+  return reps === 1 ? weight : Math.round(unRounded1RM / 2.5, 1) * 2.5;
+};
 
 function exercise() {
-  const { exercise, user, pr, oneRepMax } = useLoaderData()
+  const { exercise, user, pr, oneRepMax } = useLoaderData();
 
-  const latestPr = pr[0]
-  let currentEstimatedPr = null
+  const latestPr = pr[0];
+  let currentEstimatedPr = null;
 
   if (latestPr === undefined || pr.length === 0) {
-    console.log('no pr')
+    console.log("no pr");
   } else {
-    currentEstimatedPr = OneRmEstimate(latestPr.weight, latestPr.reps)
+    currentEstimatedPr = OneRmEstimate(latestPr.weight, latestPr.reps);
   }
 
-  let [count, setPage] = useState(0)
+  let [count, setPage] = useState(0);
 
   return (
-    <div className=''>
-      <div className=''>
+    <div className="">
+      <div className="">
         {count === 1 && (
           <div>
-            <form method='POST' className=''>
+            <form method="POST" className="">
               <input
-                className=''
-                name='_rename'
-                type='text'
+                className=""
+                name="_rename"
+                type="text"
                 placeholder={exercise.title}
               />
-              <button type='submit' className=''>
+              <button type="submit" className="">
                 Save
               </button>
             </form>
@@ -111,10 +111,10 @@ function exercise() {
         )}
         {count === 0 && (
           <div>
-            <h1 className='' onClick={() => setPage((count = 1))}>
+            <h1 className="" onClick={() => setPage((count = 1))}>
               {exercise.title}
             </h1>
-            <a className='menu-item' onClick={() => setPage((count = 1))}>
+            <a className="menu-item" onClick={() => setPage((count = 1))}>
               Rename
             </a>
           </div>
@@ -126,29 +126,29 @@ function exercise() {
             <h4>Best estimated PR recorded: {oneRepMax}kg</h4>
           </>
         ) : null}
-        <Link to='/dashboard' className=''>
+        <Link to="/dashboard" className="">
           Back
         </Link>
       </div>
       {pr.length > 0 ? (
         <div>
           <PrTable prs={pr} />
-          <Link to='./pr-new'>New PR</Link>
+          <Link to="./pr-new">New PR</Link>
           <Chart pr={pr} />
         </div>
       ) : (
         <h1>no prs yet :(</h1>
       )}
-      <div className=''>
+      <div className="">
         {user.id === exercise.userId && (
-          <form method='POST'>
-            <input type='hidden' name='_method' value='delete' />
-            <button className=''>Delete exercise</button>
+          <form method="POST">
+            <input type="hidden" name="_method" value="delete" />
+            <button className="">Delete exercise</button>
           </form>
         )}
       </div>
     </div>
-  )
+  );
 }
 
-export default exercise
+export default exercise;

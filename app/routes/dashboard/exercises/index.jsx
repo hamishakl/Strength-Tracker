95ab6b3d19dbf97dfd9a1236b319e84df15ce830
Hamish Henare <hamishjustin7@gmail.com>
2022-05-31 13:22:06 +1200

    exercises render on exercise index page

--- app/routes/dashboard/exercises/index.jsx
+++ app/routes/dashboard/exercises/index.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import { Link, useLoaderData } from "remix"
+import { db } from "~/utils/db.server"
+import { getUser } from "~/utils/session.server"
+import MyExercise from '../../../components/MyExercises'
+
+export const loader = async ({ request }) => {
+  const user = await getUser(request)
+  const exercises = {
+    exercises: await db.exercise.findMany({
+      where: {
+        userId: {
+          equals: `${user.id}`,
+        },
+      },
+      // orderBy: { createdAt: 'desc' },
+      include: {
+        Pr: {
+          select: {
+            weight: true,
+            reps: true,
+          },
+          orderBy: { weight: 'desc' },
+        },
+      },
+      orderBy: { updatedAt: 'desc' },
+    }),
+  }
+
+  return exercises
+}
 
 export default function index() {
+  const data = useLoaderData()
+  console.log(data)
   return (
-    <div>All exercises</div>
+    <>
+      <h1>My exercises</h1>
+      <MyExercise exercises={data['exercises']} />
+    </>
   )
 }

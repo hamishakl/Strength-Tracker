9bf9021013d62cb72d86975ca2dba97a469b68b0
Hamish Henare <60582221+hamishakl@users.noreply.github.com>
2022-05-31 15:49:18 +1200

    Revert "Registration"

--- app/routes/dashboard/workouts/index.jsx
+++ app/routes/dashboard/workouts/index.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
-import Navbar from '~/components/ui/PagesNavbar'
-import Masonry from 'react-masonry-css'
-
 
 export default function index() {
   return (
-    <>
-      <Navbar data={['My Workouts', 'workouts/new', 'Workout']} />
-    </>
+    <div>All workouts</div>
   )
 }

20aa7e25a8fa42f132d0f4357c2498bf6741c3f1
Hamish Henare <hamishjustin7@gmail.com>
2022-06-01 15:26:56 +1200

    moved new goal feature to own page to make room for graph content

--- app/routes/dashboard/goals/index.jsx
+++ app/routes/dashboard/goals/index.jsx
@@ -2,7 +2,8 @@ import { useLoaderData } from "@remix-run/react";
 import MyGoals from "~/components/MyGoals";
 import { db } from "~/utils/db.server";
 import { getUser } from "~/utils/session.server";
-import NewGoal from "./newGoal";
+import NewGoal from "./new";
+import Navbar from "../../../components/ui/PagesNavbar";
 
 export const loader = async ({ request }) => {
   const user = await getUser(request);
@@ -43,7 +44,7 @@ export default function index() {
   return (
     <>
       <div>
-        <h1>My goals</h1>
+        <Navbar data={['My Goals', 'goals/new', 'New Goal']} />
         <MyGoals data={notAchieved} />
         {achievedArr.length != 0 ? (
           <>
@@ -53,7 +54,7 @@ export default function index() {
         ) : null}
       </div>
       <div>
-          <NewGoal data={newGoalData}/>
+          {/* <NewGoal data={newGoalData}/> */}
       </div>
     </>
   );

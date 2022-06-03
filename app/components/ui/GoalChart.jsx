176246578d8294a16ac4b9cd456b5bb89d349f39
Hamish Henare <hamishjustin7@gmail.com>
2022-06-03 18:21:25 +1200

    tidied up goal index page,

--- app/components/ui/GoalChart.jsx
+++ app/components/ui/GoalChart.jsx
@@ -1,38 +1,60 @@
 import React from 'react'
-import { PieChart, Pie, Sector, Cell, Tooltip,} from 'recharts'
+import { PieChart, Pie, Sector, Cell, Tooltip } from 'recharts'
 
-// const data = [
-//   { name: 'Group A', value: 400 },
-//   { name: 'Group B', value: 300 },
-//   { name: 'Group C', value: 300 },
-//   { name: 'Group D', value: 200 },
-// ]
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042']
 
-export default function GoalChart(chartData) {
-  console.log(chartData)
-  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-    const RADIAN = Math.PI / 180;
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-    const x = cx + radius * Math.cos(-midAngle * RADIAN);
-    const y = cy + radius * Math.sin(-midAngle * RADIAN);
-  
+const CustomTooltip = ({ active, payload, label }) => {
+  if (active && payload && payload.length) {
     return (
-      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+        <div className="custom-tooltip">
+        {payload[0].name === 'Remaining' ? (
+            <p>{Math.round(payload[0].value)}kg to go</p>
+        ) : (
+            <p>{Math.round(payload[0].value)}kg so far</p>
+        )}     
+        </div>
+    )
+  }
+
+  return null
+}
+
+export default function GoalChart(chartData) {
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const RADIAN = Math.PI / 180
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5
+    const x = cx + radius * Math.cos(-midAngle * RADIAN)
+    const y = cy + radius * Math.sin(-midAngle * RADIAN)
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central"
+      >
         {`${(percent * 100).toFixed(0)}%`}
       </text>
-    );
-  };
+    )
+  }
   return (
-    <div>
+    <div className={'goal-pie-chart__wrapper'}>
       {chartData.chartData.map((data) => {
         const progress = 100 - ((data.goal - data.current) / data.goal) * 100
-        console.log(data)
-        let remaining 
-        if (progress >=100) {
-            remaining = 0
+        let remaining
+        if (progress >= 100) {
+          remaining = 0
         } else {
-            remaining = 100 - progress
+          remaining = 100 - progress
         }
         const dataArray = [
           {
@@ -45,7 +67,9 @@ export default function GoalChart(chartData) {
           },
         ]
         return (
-            <PieChart width={400} height={400}>
+            <div className='goal-pie-chart__div'>
+            <h3>{data.name}</h3>
+          <PieChart width={400} height={400}>
             <Pie
               data={dataArray}
               cx="50%"
@@ -55,13 +79,17 @@ export default function GoalChart(chartData) {
               outerRadius={80}
               fill="#8884d8"
               dataKey="value"
-            >
+              >
               {dataArray.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-              ))}
+                  <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                  />
+                  ))}
             </Pie>
-            <Tooltip />
+            <Tooltip content={<CustomTooltip />} />
           </PieChart>
+                  </div>
         )
       })}
     </div>

de238753a81a6f66d2ce7213b2dce0c333dc7ca1
Hamish Henare <hamishjustin7@gmail.com>
2022-06-08 15:30:25 +1200

    mobile nav drawer renders throughout application now... state is used to show and hide it;

---
+++ app/components/ui/MobileNav.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function MobileNav({children}) {
+
+    // const count = data[0]
+    // let [count, setPage] = useState(0);
+    return (
+    <div className="mobile-nav-menu__wrapper">
+        <p>hello</p>
+        {children}
+    </div>
+  )
+}

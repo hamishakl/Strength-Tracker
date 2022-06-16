fd314f445361b27c760d2be004618b5538a3b1ac
Hamish Henare <hamishjustin7@gmail.com>
2022-06-16 15:03:36 +1200

    added direct pr link for individual exercises

--- app/components/ui/PrNav.jsx
+++ app/components/ui/PrNav.jsx
@@ -1,6 +1,7 @@
 import { Link } from '@remix-run/react'
 
 export default function PrNavbar(data) {
+  console.log(data)
   const str = data.data[0]
   const strSplit = str.split(' ')
   const id = strSplit[strSplit.length - 1]
@@ -17,6 +18,9 @@ export default function PrNavbar(data) {
     <div className={'dashboard-section-navbar'}>
       <Link to={`../dashboard/exercises/${id}`}>
         <h2 className="">{title}</h2>
+      </Link>
+      <Link to={`../dashboard/exercises/${id}/pr-new`}>
+        <p className="">New PR</p>
       </Link>
     </div>
   )

e4bcbb02c7f0cd278a3d9040e222aca108bef8c9
Hamish Henare <hamishjustin7@gmail.com>
2022-06-16 18:32:58 +1200

    switching pc

--- app/routes/dashboard/exercises/$exerciseId.pr-new.jsx
+++ app/routes/dashboard/exercises/$exerciseId.pr-new.jsx
@@ -1,7 +1,8 @@
-import { json, redirect } from "@remix-run/node";
-import { Form, Link, useActionData, useLoaderData } from "@remix-run/react";
+import { json, redirect } from '@remix-run/node'
+import { Form, Link, useActionData, useLoaderData } from '@remix-run/react'
 import { db } from '~/utils/db.server'
 import { getUser } from '~/utils/session.server'
+import { goalCalc } from '../prs/new'
 
 function validateWeight(weight) {
   if (typeof weight !== 'number') {
@@ -35,6 +36,8 @@ export const action = async ({ request, params }) => {
   const weight = parseInt(weightStr)
   const reps = parseInt(repsStr)
   const exerciseId = params.exerciseId
+  const dateStr = form.get('date')
+  const date = new Date(dateStr).toISOString()
 
   const user = await getUser(request)
 
@@ -51,8 +54,27 @@ export const action = async ({ request, params }) => {
   }
 
   const pr = await db.pr.create({
-    data: { ...fields, userId: user.id, exerciseId: exerciseId },
+    data: { ...fields, userId: user.id, exerciseId: exerciseId , date: date},
   })
+
+  const goals = await db.goals.findMany({
+    where: { userId: user.id, exerciseId: exerciseId },
+  })
+  const goalWeight = goals[0].weight
+  const goalReps = goals[0].reps
+  const goalId = goals[0].id
+
+  const results = goalCalc(weight, reps, goalWeight, goalReps)
+
+  if (results[0] === true) {
+    await db.goals.update({
+      where: { id: goalId },
+      data: {
+        achieved: true,
+        achievementDate: date,
+      },
+    })
+  }
 
   return redirect(`/dashboard/exercises/${exerciseId}`)
 }
@@ -63,34 +85,46 @@ function NewPr() {
 
   return (
     <>
-      <div className='page-header'>
+      <div className="page-header">
         <h1>New PR for {exercise.title}</h1>
-        <Link to='/dashboard' className=''>
+        <Link to="/dashboard" className="">
           Back
         </Link>
       </div>
-      <div className=''>
-        <Form method='POST'>
-          <div className=''>
-            <label htmlFor='weight'>weight</label>
-            <input type='number' name='weight' id='weight' />
-            <div className=''>
+      <div className="">
+        <Form method="POST">
+        <div>
+          <h5>Date of workout:</h5>
+          <input
+            type='date'
+            id='start'
+            name='date'
+            defaultValue={date}
+            min={userJoinDate}
+            max={date}
+            required
+          ></input>
+        </div>
+          <div className="">
+            <label htmlFor="weight">weight</label>
+            <input type="number" name="weight" id="weight" />
+            <div className="">
               <p>
                 {actionData?.fieldErrors?.weight &&
                   actionData?.fieldErrors?.weight}
               </p>
             </div>
           </div>
-          <div className=''>
-            <label htmlFor='reps'>Exercise reps</label>
-            <input type='number' name='reps' id='reps' />
-            <div className=''>
+          <div className="">
+            <label htmlFor="reps">Exercise reps</label>
+            <input type="number" name="reps" id="reps" />
+            <div className="">
               <p>
                 {actionData?.fieldErrors?.reps && actionData?.fieldErrors?.reps}
               </p>
             </div>
           </div>
-          <button type='submit' className=''>
+          <button type="submit" className="">
             Add PR
           </button>
         </Form>

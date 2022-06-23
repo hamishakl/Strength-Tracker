1fbe5257ade3b2d988dc90888ec71141d390807a
Hamish Henare <hamishjustin7@gmail.com>
2022-06-24 10:59:49 +1200

    goals index is mobile friendly now

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {},
-    minHeight: {
-      '1/2': '50vh',
-    }, 
-    height: {      
-      '1/2': '25vh'
+    extend: {
+
+      minHeight: {
+        '1/2': '50vh',
+        'full': '100%',
+        'screen': '100vh',
+        '80': '80%'
+      }, 
+      height: {      
+        '1/2': '25vh',
+      'full': '100%',
+      'screen': '100vh',
+      '80': '80%'
     }
+  },
   },
   plugins: [],
 };

ab389a45fcf364e375de437f0899f54ab79dfce7
Hamish Henare <hamishjustin7@gmail.com>
2022-06-27 12:13:54 +1200

    updated ui for new pr form

--- app/routes/dashboard/prs/new.jsx
+++ app/routes/dashboard/prs/new.jsx
@@ -4,13 +4,13 @@ import { getUser } from '~/utils/session.server'
 import { db } from '~/utils/db.server'
 import Navbar from '~/components/ui/PagesNavbar'
 
-function validateWeight(weight) {
+function validateweight(weight) {
   if (typeof weight !== 'number') {
     return 'weight should be a number'
   }
 }
 
-function validateReps(reps) {
+function validatereps(reps) {
   if (typeof reps !== 'number') {
     return 'reps should be a number'
   }
@@ -35,7 +35,7 @@ function oneRmCalc(weight, reps) {
   } else return Math.floor(weight * reps * 0.0333 + weight, 2.5)
 }
 
-export function goalCalc(weight, reps, goalWeight, goalReps) {
+export function goalCalc(weight, reps, goalweight, goalreps) {
   const preplinsTable = {
     100.0: 1,
     95.5: 2,
@@ -54,13 +54,13 @@ export function goalCalc(weight, reps, goalWeight, goalReps) {
   const entries = Object.entries(preplinsTable)
   let percentageStr
   for (let i = 0; i < entries.length; i++) {
-    if (goalReps < entries[i][1]) {
+    if (goalreps < entries[i][1]) {
       percentageStr = entries[i][0]
     }
   }
   let percentage = parseInt(percentageStr) / 100
   let current = onerm * percentage
-  let goal = oneRmCalc(goalWeight, goalReps) * percentage
+  let goal = oneRmCalc(goalweight, goalreps) * percentage
   let progress = (current / goal) * 100
   let remainingPercent = 100 - progress
   let remainingKg = goal - current
@@ -84,15 +84,15 @@ export const action = async ({ request, params }) => {
   const goals = await db.goals.findMany({
     where: { userId: user.id, exerciseId: id },
   })
-  const goalWeight = goals[0].weight
-  const goalReps = goals[0].reps
+  const goalweight = goals[0].weight
+  const goalreps = goals[0].reps
   const goalId = goals[0].id
 
   const fields = { weight, reps }
 
   const fieldErrors = {
-    weight: validateWeight(weight),
-    reps: validateReps(reps),
+    weight: validateweight(weight),
+    reps: validatereps(reps),
   }
 
   if (Object.values(fieldErrors).some(Boolean)) {
@@ -100,7 +100,7 @@ export const action = async ({ request, params }) => {
     return badRequest({ fieldErrors, fields })
   }
 
-  const results = goalCalc(weight, reps, goalWeight, goalReps)
+  const results = goalCalc(weight, reps, goalweight, goalreps)
 
   if (results[0] === true) {
     await db.goals.update({
@@ -159,78 +159,85 @@ const date = dateData[1]
   return (
     <div className="">
       <Navbar data={['New PR', 'prs', 'Back']} />
-      <Form method="POST">
-        <div className="">
-          <label htmlFor="exercise" className="">
-            Exercise
-          </label>
-          <select
-            className=""
-            aria-label="Default select example"
-            required
-            id="exercise"
-            name="exercise"
-          >
-            <option selected disabled>
-              Pick an exercise
-            </option>
-            {exercises.map((exercise) => (
+      <div className="">
+        <Form method="POST" className='mt-4'>
+          <div className="relative z-0 w-full mb-6 group">
+            <select
+              id="exercise"
+              name="exercise"
+              // defaultValue={'Pick an exercise'}
+              placeholder={'Pick an exercise'}
+              className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            >
+              <option disabled selected>
+                Pick an exercise
+              </option>
+              {exercises.map((exercise) => (
               <option key={exercise.id} value={exercise.id}>
                 {exercise.title}
               </option>
             ))}
-          </select>
-        </div>
-        <div className="">
-          <label htmlFor="weight" className="">
-            Weight
-          </label>
-          <input
-            type="number"
-            className=""
-            id="weight"
-            name="weight"
-            required
-          />
-          <div className="">
-            <p>
-              {actionData?.fieldErrors?.weight &&
-                actionData?.fieldErrors?.weight}
-            </p>
+            </select>
           </div>
-        </div>
-        <div className="">
-          <label htmlFor="reps" className="">
-            Reps
-          </label>
-          <input type="number" className="" name="reps" id="reps" required />
-          <div className="">
-            <p>
-              {actionData?.fieldErrors?.reps && actionData?.fieldErrors?.reps}
-            </p>
+          <div className="relative z-0 w-full mb-6 group">
+            <input
+              type="number"
+              name="weight"
+              id="weight"
+              className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="weight"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Weight
+            </label>
           </div>
-        </div>
-        <div>
-          <h5>Date of workout:</h5>
-          <input
-            type='date'
-            id='start'
-            name='date'
-            defaultValue={date}
-            min={userJoinDate}
-            max={date}
-            required
-          ></input>
-        </div>
-
-        <div className="">
-          <h5>Projected 1rm: </h5>
-          <p></p>
-        </div>
-        <button type="submit" className="">
-          Submit
-        </button>
-      </Form>
+          <div className="relative z-0 w-full mb-10 group">
+            <input
+              type="number"
+              name="reps"
+              id="reps"
+              className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="reps"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Reps
+            </label>
+          </div>
+          <div className="relative z-0 w-full mb-6 mt-2 group">
+            <input
+              type="date"
+              name="date"
+              id="date"
+              className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+              defaultValue={date}
+              min={userJoinDate}
+              max={date}
+            />
+            <label
+              for="date"
+              className="peer-focus:font-medium absolute mb-2 text-gray-500 text-lg dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 text-left"
+            >
+              Date
+            </label>
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
+        </Form>
+      </div>
     </div>
   )
 }

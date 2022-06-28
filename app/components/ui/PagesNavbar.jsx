6f5ad7a2b613dcc7baa344a004bf12fc03db003d
Hamish Henare <hamishjustin7@gmail.com>
2022-06-29 03:59:03 +1200

    tidied up exercise index

--- app/components/ui/PagesNavbar.jsx
+++ app/components/ui/PagesNavbar.jsx
@@ -2,9 +2,9 @@ import { Link } from "@remix-run/react";
 
 export default function Navbar(data) {
   return (
-      <div className={'dashboard-section-navbar flex justify-between'}>
+      <div className={'dashboard-section-navbar flex justify-between items-center mb-4'}>
         <h2 className='text-2xl font-bold'>{data.data[0]}</h2>
-          <Link to={`/dashboard/${data.data[1]}`} className=''>
+          <Link to={`/dashboard/${data.data[1]}`} className='underline'>
             <span>{data.data[2]}</span>
           </Link>
       </div>

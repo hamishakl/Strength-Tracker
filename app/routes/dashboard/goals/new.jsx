62d9c90d0863d0386d5e34b628073ba8aa9d6422
Hamish Henare <hamishjustin7@gmail.com>
2022-07-02 15:14:07 +1200

    adjusted new goal form datep icker range

--- app/routes/dashboard/goals/new.jsx
+++ app/routes/dashboard/goals/new.jsx
@@ -121,9 +121,8 @@ export default function newPr() {
               className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
               placeholder=" "
               required
-              defaultValue={date}
+              defaultValue={userJoinDate}
               min={userJoinDate}
-              max={date}
             />
             <label
               for="date"

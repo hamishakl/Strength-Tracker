cec22c4a03b8f59ff9a8e9687905499bb6ea4a72
Hamish Henare <hamishjustin7@gmail.com>
2022-07-04 06:11:04 +1200

    changed the meta to disable zooming in on safari

--- app/root.jsx
+++ app/root.jsx
@@ -55,7 +55,7 @@ export function ErrorBoundary(error) {
     <html lang="en">
       <head>
         <title>Oh no!</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <Links />
       </head>
       <body>
